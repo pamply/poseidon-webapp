e884cd91095d48ecde56233953d0504c9e21af20
Manuel Pamplona <Manuel.Pamplona.Rosado@gmail.com>
2017-09-19 10:45:53 -0500

    Adding dist for testing purposes

---
+++ dist/build.js
@@ -0,0 +1,1 @@
+console.error("SyntaxError: /Users/manuelpamplona/dev/poseidon-webapp/src/main.js: Unexpected token, expected ; (3:27) while parsing file: /Users/manuelpamplona/dev/poseidon-webapp/src/main.js");
